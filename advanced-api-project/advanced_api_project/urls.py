7661be1d03b06bc1f16d7f7d0f6415556973d0d9
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2025-09-28 19:28:27 +0300

    Update urls.py

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # <-- Add this line to include your app's URLs
 ]
+
+# This configuration ensures that all endpoints defined in api/urls.py are accessible under /api/
