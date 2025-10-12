9c972cb4634e53ea7ac9630e16037a3316bbeafa
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2025-10-12 18:07:01 +0300

    Delete social_media_api directory

--- social_media_api/settings.py
+++
@@ -1,7 +0,0 @@
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-}
