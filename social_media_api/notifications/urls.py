27b93647df0a9bde67f82217323cb62a9938b954
Tsion Shimelis <153851690+TsiOnshime@users.noreply.github.com>
2025-10-12 20:25:15 +0300

    Delete social_media_api directory

--- social_media_api/notifications/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import NotificationListView, UnreadNotificationListView
-
-app_name = 'notifications'  # Add this line for namespacing
-
-urlpatterns = [
-    path('', NotificationListView.as_view(), name='notifications'),
-    path('unread/', UnreadNotificationListView.as_view(), name='unread-notifications'),
-]
